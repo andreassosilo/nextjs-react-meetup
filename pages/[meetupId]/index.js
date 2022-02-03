0672847cbc651a776313019a11281358c593277f
Andreas Sosilo <andreas.sosilo@gmail.com>
2022-02-03 22:56:51 +0100

    Adding custom components & CSS modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://www.tripsavvy.com/thmb/vH1qnFyjFWkNrx9LgGWabSoq98c=/1414x1414/smart/filters:no_upscale()/GettyImages-962642536-33584e0b843c4c2ba25e7e7922c97745.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is the first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
